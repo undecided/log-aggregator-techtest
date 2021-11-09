a8df80ae31e68944c2db1e6d2846151766231a94
Matthew Bennett-Lovesey <matt+gitcommits@blltd.uk>
2021-11-09 18:12:12 +0000

    Basic functionality acheived - needs corner cases and refactoring

--- lib/log_aggregator.rb
+++ lib/log_aggregator.rb
@@ -6,10 +6,17 @@ class LogAggregator
   class Error < StandardError; end
 
   def initialize(filename)
-    @file_lines = File.read(filename).lines
+    @file_lines = File.read(filename)
+                      .lines
+                      .map { |line| line.strip.split(/\s+/) }
   end
 
   def display_by_frequency!
-    puts @file_lines
+    # We could use Array#tally here, however we'll do it the hard way for
+    # backwards compatability
+    @file_lines.each_with_object({}) do |path_and_ip_arr, hash|
+      hash[path_and_ip_arr.first] ||= 0
+      hash[path_and_ip_arr.first] += 1
+    end.map { |path_and_tally| path_and_tally.join(" ")}
   end
 end
--- spec/log_aggregator_spec.rb
+++ spec/log_aggregator_spec.rb
@@ -1,5 +1,17 @@
-RSpec.describe LogAggregator do
-  it "has a version number" do
-    expect(LogAggregator::VERSION).not_to be nil
+describe LogAggregator do
+  describe "#display_by_frequency" do
+    context "given a list of web pages with ip addresses" do
+      let(:log_filename) { "./spec/fixtures/simplified_happy_path.log" }
+      subject { described_class.new(log_filename).display_by_frequency! }
+
+      it "presents a single line for each web page given" do
+        expect(subject).to eq [
+          "/about 2",
+          "/help_page/1 2",
+          "/index 1",
+        ]
+      end
+    end
+
   end
 end
