e878a70a2b52196fbb378e362604d8ab376b1623
Matthew Bennett-Lovesey <matt+gitcommits@blltd.uk>
2021-11-10 18:19:17 +0000

    Upgrade tally to enable unique ip sources

--- lib/log_aggregator/tally.rb
+++ lib/log_aggregator/tally.rb
@@ -1,16 +1,23 @@
 module LogAggregator
+  # We could leverage Array#tally here, however we'll do it the hard way for
+  # backwards compatability
   class Tally
     def initialize(ingester)
       @paths_with_connection_source = ingester.to_a
     end
 
-    # We could leverage Array#tally here, however we'll do it the hard way for
-    # backwards compatability
     def to_hash
-      @paths_with_connection_source.each_with_object({}) do |path_and_ip, hash|
-        hash[path_and_ip.first] ||= 0
-        hash[path_and_ip.first] += 1
+      @paths_with_connection_source.each_with_object({}) do |(path, ip), hash|
+        hash[path] ||= 0
+        hash[path] += 1
       end
+    end
+
+    def to_unique_hash
+      @paths_with_connection_source.each_with_object({}) do |(path, ip), hash|
+        hash[path] ||= []
+        hash[path] |= [ip] # set union, not or-equals
+      end.transform_values { |v| v.count }
     end
   end
 end
--- spec/lib/tally_spec.rb
+++ spec/lib/tally_spec.rb
@@ -1,10 +1,27 @@
 describe LogAggregator::Tally do
+  let(:input) do
+    [
+      ["/foo/bar", "1.1.1.1"],
+      ["/foo/bar", "1.1.1.1"],
+      ["/baz/1", "1.1.1.1"],
+      ["/baz/1", "1.1.1.2"]
+    ]
+  end
+
   describe "#to_hash" do
     subject { described_class.new(input).to_hash }
-    let(:input) { [] }
 
     it "produces a hash mapping paths to their frequency" do
-      expect(subject).to be_an Hash
+      expect(subject).to eq({"/baz/1"=>2, "/foo/bar"=>2})
     end
   end
+
+  describe "#to_unique_hash" do
+    subject { described_class.new(input).to_unique_hash }
+
+    it "produces a hash mapping paths to their unique frequency" do
+      expect(subject).to eq({"/baz/1"=>2, "/foo/bar"=>1})
+    end
+  end
+
 end
